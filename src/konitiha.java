5dcab85d4727f4645488d7ca60acdfbe4028c754
0609ryu <ryuukimura30@gmail.com>
2024-02-02 22:20:40 +0900

    first commit

---
+++ src/konitiha.java
@@ -0,0 +1,25 @@
+
+public class konitiha {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//こんにちはと表示する
+		System.out.println("こんにちは");
+		
+		//123と表示する
+		System.out.println("123");
+		
+		//おはようと表示する
+		System.out.println("おはよう");
+		
+		//例外が発生しましたと表示する
+		System.out.println("例外が発生しました。");
+		
+		//例外が　発生しましたと表示する
+		System.out.println("例外が");
+		System.out.println("発生しました。");
+
+	}
+
+}
